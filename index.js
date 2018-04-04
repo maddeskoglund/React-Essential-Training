7fded14f1aec08d13a84b8797457e31b052d842a
maddeskoglund <madelein.blomster@hotmail.com>
2018-04-04 21:24:35 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { createElement } = React;
+const { render } = ReactDOM;
+
+const style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'verdana'
+};
+
+const title = createElement(
+  'h1',
+  { id: 'title', className: 'header', style },
+  'Hello World'
+);
+
+render(title, document.getElementById('react-container'));
